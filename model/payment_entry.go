51e6d51ab2914541cfc476c73d3ea48242713594
Carl <carl@gmail.com>
2022-03-29 23:41:45 +0400

    文件合并

--- model/payment_entry.go
+++ model/payment_entry.go
@@ -15,7 +15,21 @@ import (
 	"github.com/valyala/fasthttp"
 )
 
-// 定义全局 payment
+// Payment 接口
+type Payment interface {
+	// Name 支付通道名称
+	Name() string
+	// New 初始化 通道配置
+	New()
+	// Pay 发起支付
+	Pay(log *paymentTDLog, ch paymentChannel, amount, bid string) (paymentDepositResp, error)
+	// Withdraw 发起代付
+	Withdraw(log *paymentTDLog, param WithdrawAutoParam) (paymentWithdrawalRsp, error)
+	// PayCallBack 支付回调
+	PayCallBack(ctx *fasthttp.RequestCtx) (paymentCallbackResp, error)
+	// WithdrawCallBack 代付回调
+	WithdrawCallBack(*fasthttp.RequestCtx) (paymentCallbackResp, error)
+}
 
 //New 初始化配置
 func NewPayment() {
