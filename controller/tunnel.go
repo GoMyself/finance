d5523b805cdcd358b94bdaeae1e4afe91f05cf1f
Linch <Linch@gmail.com>
2022-05-15 17:34:52 +0800

    xg

--- controller/tunnel.go
+++ controller/tunnel.go
@@ -10,11 +10,11 @@ import (
 
 type TunnelController struct{}
 
-type updateTunnelParam struct {
+type tunnelUpdateParam struct {
 	ID    string `rule:"digit" msg:"id error" name:"id"`                        // id
 	Sort  string `rule:"digit" min:"1" max:"99" msg:"sort error" name:"sort"`   // 排序
 	Value string `rule:"float" min:"1" max:"99" msg:"value error" name:"value"` // 排序
-	State string `rule:"digit" min:"1" max:"99" msg:"state error" name:"state"` // 排序
+	State string `rule:"digit" min:"0" max:"1" msg:"state error" name:"state"`  // 排序
 	Code  string `rule:"digit" msg:"code error" name:"code"`                    // 动态验证码
 }
 
@@ -33,7 +33,7 @@ func (that *TunnelController) List(ctx *fasthttp.RequestCtx) {
 // Update 财务管理-渠道管理-通道类型管理-修改
 func (that *TunnelController) Update(ctx *fasthttp.RequestCtx) {
 
-	param := updateTunnelParam{}
+	param := tunnelUpdateParam{}
 	err := validator.Bind(ctx, &param)
 	if err != nil {
 		helper.Print(ctx, false, helper.ParamNull)
